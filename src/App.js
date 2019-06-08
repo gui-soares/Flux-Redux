50c59dc97620cb753d0236f94a333df2ad0b02e3
gui-soares <eng.guilhermemsoares@gmail.com>
2019-06-08 17:04:53 -0300

    Reactotron

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import './config/ReactotronConfig';
+
 import { Provider } from 'react-redux';
 
 import store from './store';
 
 import TodoList from './TodoList';
+
+console.tron.log({ Hello: 'Testando...' });
 
 const App = () => (
   <Provider store={store}>
